8d8c4a7733c403d3d89d6bfdd4bdbcbc62f8706b
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-02-14 22:21:46 -0600

    first commit

---
+++ 02_Variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Juan Ticante"
+	var age int
+
+	lastName := "Vicente"
+
+	age = 32
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(lastName)
+}
